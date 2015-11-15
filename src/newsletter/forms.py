f46f0d64912bb70d59d87618c0a9a87f9c7f80b3
LiuWenJu <lwj037303@gmail.com>
2015-11-15 22:11:56 +0800

    update

---
+++ src/newsletter/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from .models import SignUp
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        medel = SignUp
+        fields = ['full_name', 'email']
+        #exclude = ['full_name']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        domain, extension = provider.split('.')
+        if not extension == "usc":
+            raise forms.ValidationError("Please user a valid .usc email address")
+        if not extension == "edu":
+            raise forms.ValidationError("Please use a valid .edu email address")
+        return email
+
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get('full_name')
+        return full_name
--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    title = "Welcome!"
+    if request.user.is_authenticated():
+        title = "My Title %s"%(request.user)
 
-    return render(request, "home.html", {})
+    context = {
+        "template_title": title,
+        #"abc": 123,
+    }
+    return render(request, "home.html", context)
+
