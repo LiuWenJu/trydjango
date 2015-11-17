4656dececb36f0f9b9667fbd36e16f20a661996b
LiuWenJu <lwj037303@gmail.com>
2015-11-17 12:06:40 +0800

    add and change files

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-#from someotherapp.models import someotherMedels
 
 from .forms import SignUpForm
 from .models import SignUp
 
 class SignUpAdmin(admin.ModelAdmin):
-    list_display = ["__unicode__", "timestamp", "updated"]
-    form = SignUpForm
-    #class Meta:
-    #    model = SignUp
+	list_display = ["__unicode__", "timestamp", "updated"]
+	form = SignUpForm
+	# class Meta:
+	# 	model = SignUp
+
+
+
 admin.site.register(SignUp, SignUpAdmin)
-
-
-
